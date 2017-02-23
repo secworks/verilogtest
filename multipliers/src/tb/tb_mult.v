d417a523d19db63543d598e43094b9c659983da2
Joachim Strömbergson <joachim@secworks.se>
2017-02-23 21:06:41 +0100

    Debugged the tb. Now it builds. Time to add test cases.

--- multipliers/src/tb/tb_mult.v
+++ multipliers/src/tb/tb_mult.v
@@ -41,6 +41,11 @@ module tb_mult;
   //----------------------------------------------------------------
   // Internal constant and parameter definitions.
   //----------------------------------------------------------------
+  parameter DEBUG     = 0;
+
+  parameter CLK_HALF_PERIOD = 1;
+  parameter CLK_PERIOD      = 2 * CLK_HALF_PERIOD;
+
   parameter API_WIDTH  = 16;
   localparam OPA_WIDTH = 64;
   localparam OPB_WIDTH = 64;
@@ -56,6 +61,12 @@ module tb_mult;
   reg  [7 : 0]               tb_address;
   reg  [(API_WIDTH - 1) : 0] tb_write_data;
   wire [(API_WIDTH - 1) : 0] tb_read_data;
+
+  reg [(API_WIDTH - 1) : 0]  read_data;
+
+  reg [31 : 0]               cycle_ctr;
+  reg [31 : 0]               error_ctr;
+  reg [31 : 0]               tc_ctr;
 
 
   //----------------------------------------------------------------
@@ -147,22 +158,6 @@ module tb_mult;
   task dump_dut_state;
     begin
       $display("cycle:  0x%016x", cycle_ctr);
-      $display("ctrl:   init_reg = 0x%01x, next_reg = 0x%01x, finalize_reg = 0x%01x",
-               dut.init_reg, dut.next_reg, dut.finalize_reg);
-      $display("config: keylength = 0x%01x, final blocklength = 0x%01x",
-               dut.keylen_reg, dut.final_size_reg);
-      $display("k1 = 0x%016x, k2 = 0x%016x", dut.k1_reg, dut.k2_reg);
-      $display("ready = 0x%01x, valid = 0x%01x, result_we = 0x%01x, block_mux = 0x%02x, ctrl_state = 0x%02x",
-               dut.ready_reg, dut.valid_reg, dut.result_we, dut.bmux_ctrl, dut.cmac_ctrl_reg);
-      $display("block:  0x%08x%08x%08x%08x",
-               dut.block_reg[0], dut.block_reg[1], dut.block_reg[2], dut.block_reg[3]);
-      $display("tweaked_block: 0x%032x", dut.cmac_datapath.tweaked_block);
-      $display("result: 0x%032x", dut.result_reg);
-      $display("");
-      $display("core_init = 0x%01x, core_next = 0x%01x, core_ready = 0x%01x, core_valid = 0x%01x",
-               dut.core_init, dut.core_next, dut.core_ready, dut.core_valid);
-      $display("core block:  0x%032x", dut.core_block);
-      $display("core result: 0x%032x", dut.core_result);
       $display("");
     end
   endtask // dump_dut_state
@@ -186,7 +181,7 @@ module tb_mult;
       tb_cs         = 0;
       tb_we         = 0;
       tb_address    = 8'h0;
-      tb_write_data = 32'h0;
+      tb_write_data = 16'h0;
     end
   endtask // init_sim
 
